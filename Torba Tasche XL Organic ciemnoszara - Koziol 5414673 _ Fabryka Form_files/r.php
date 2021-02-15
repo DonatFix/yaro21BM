91ae3348db13db67cbddb1a326413c1fa3d2c1b7
DonatFix <78790942+DonatFix@users.noreply.github.com>
2021-02-15 12:59:51 +0200

    d
    d

---
+++ Torba Tasche XL Organic ciemnoszara - Koziol 5414673 _ Fabryka Form_files/r.php
@@ -0,0 +1,18 @@
+try{
+recommendations = []
+recommendations.push(["70431" ,"https://fabrykaform.pl/https://fabrykaform.pl/koziol-torba-taschelino-organic?qrtc_slt=product_frame&qrtc_tpl=tpl_product_js&qrtc_typ=prd&qrtc_prd=70699&qrtc_pos=1"]);
+recommendations.push(["70435" ,"https://fabrykaform.pl/https://fabrykaform.pl/koziol-torba-taschelini-organic?qrtc_slt=product_frame&qrtc_tpl=tpl_product_js&qrtc_typ=prd&qrtc_prd=70699&qrtc_pos=2"]);
+recommendations.push(["10280" ,"https://fabrykaform.pl/https://fabrykaform.pl/koziol-torba-tasche-xl?qrtc_slt=product_frame&qrtc_tpl=tpl_product_js&qrtc_typ=prd&qrtc_prd=70699&qrtc_pos=3"]);
+recommendations.push(["10257" ,"https://fabrykaform.pl/https://fabrykaform.pl/koziol-torba-taschelino?qrtc_slt=product_frame&qrtc_tpl=tpl_product_js&qrtc_typ=prd&qrtc_prd=70699&qrtc_pos=4"]);
+recommendations.push(["10288" ,"https://fabrykaform.pl/https://fabrykaform.pl/koziol-torba-taschelini?qrtc_slt=product_frame&qrtc_tpl=tpl_product_js&qrtc_typ=prd&qrtc_prd=70699&qrtc_pos=5"]);
+recommendations.push(["31818" ,"https://fabrykaform.pl/https://fabrykaform.pl/reisenthel-torba-nestbasket?qrtc_slt=product_frame&qrtc_tpl=tpl_product_js&qrtc_typ=prd&qrtc_prd=70699&qrtc_pos=6"]);
+recommendations.push(["77707" ,"https://fabrykaform.pl/https://fabrykaform.pl/fatboy-torba-sjopper-kees?qrtc_slt=product_frame&qrtc_tpl=tpl_product_js&qrtc_typ=prd&qrtc_prd=70699&qrtc_pos=7"]);
+recommendations.push(["45056" ,"https://fabrykaform.pl/https://fabrykaform.pl/stackers-szkatulka-na-bizuterie-stackers-potrojna-classic?qrtc_slt=product_frame&qrtc_tpl=tpl_product_js&qrtc_typ=prd&qrtc_prd=70699&qrtc_pos=8"]);
+recommendations.push(["6630" ,"https://fabrykaform.pl/https://fabrykaform.pl/kitchenaid-czajnik-elektryczny-artisan-1-5-l?qrtc_slt=product_frame&qrtc_tpl=tpl_product_js&qrtc_typ=prd&qrtc_prd=70699&qrtc_pos=9"]);
+
+quartic_rec( recommendations ) // funkcja klienta
+
+} catch(err){
+    try{console.log('[Quartic template] '+err)}
+    catch(err_2){}
+}
